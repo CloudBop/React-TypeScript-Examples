fb9dd4baa193105826d39fb24320d236165f2540
Cloudbop <boppincolin@gmail.com>
2021-08-06 00:46:37 +0100

    +generic + union

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -8,14 +8,31 @@ const users = [
 
 const UserSearch: React.FC = () => {
   const [name, setName] = useState("");
+  const [user, setUser] = useState<
+    // <{generic}> + union |
+    { name: string; age: number } | undefined | null
+  >(
+    //
+    null
+  );
 
-  const onClick = () => {};
+  const onClick = () => {
+    const foundUser = users.find((user) => user.name === name);
+    setUser(foundUser);
+  };
 
   return (
     <div>
       User Search
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Find User</button>
+      <div>
+        {
+          // typescript will warn of undefined errors
+          user && user.name
+        }
+        {user?.name}
+      </div>
     </div>
   );
 };
